5fccfd0f9b5e96c9bc68aab6b5d0ed8632175e14
Srikrishnan B <bsk19.05.1998@gmail.com>
2023-05-27 00:47:38 +0530

    url changed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let crypto = require('crypto');
  
  app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  });
 //  let server = http.createServer(app)
 let server = http.createServer(app)
